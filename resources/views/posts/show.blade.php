5f4c712a22e820ab6d120a8b06e0afbdf17902ec
johnnyw4r <johnny.aracena@gmail.com>
2017-08-15 14:37:00 -0300

    Tercer Commit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,9 @@
 			<h2>Title</h2>
 
 			<p>Description</p>
+			<p>Esta es una descripción de prueba para vrificar si se hace commit en github</p>
 
+				
 			<p>Posted 5 houres ago.</p>		
 
 			</div>
