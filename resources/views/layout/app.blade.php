8590a96487a8c0767f1f02544ee8c210f3e15c61
johnnyw4r <johnny.aracena@gmail.com>
2017-08-15 13:52:24 -0300

    Intento de commit desde carpeta reddit-clone clonada

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Reddit Clone</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="container">
+
+		<div class="row">
+			
+			<div class="col-md-12">
+				
+				<h1>Reddit Clone nuevo titulo Prueba segungo commit</h1>
+
+			</div>
+		</div>
+		
+		<hr>
+
+		@yield('contenido')
+
+
+	</div>
+
+</body>
+</html>
+
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.app')
+
+
+@section('contenido')
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Title</h2>
+				<p>Posted 5 houres ago.</p>		
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Title</h2>
+				<p>Posted 5 houres ago.</p>	
+			</div>
+		</div>
+			
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Title</h2>
+				<p>Posted 5 houres ago.</p>		
+
+			</div>
+		</div>
+		
+		
+@endsection
+
+
+</body>
+</html>
+
